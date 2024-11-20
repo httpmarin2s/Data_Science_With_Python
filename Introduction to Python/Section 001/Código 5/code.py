df5da817230ca27fcbf97f4441c752750e93e8bc
Marianna Ferreira <117746493+httpmarin2s@users.noreply.github.com>
2024-11-19 21:48:40 -0300

    Create code.py

---
+++ Introduction to Python/Section 001/Código 5/code.py
@@ -0,0 +1,4 @@
+# 5. Crie um programa que receba uma palavra do usuário e imprima cada letra da palavra separadamente usando um laço for.
+palavra = input("Digite uma palavra: ")
+for a in palavra: 
+  print(a)
