5fbd2fd77b12e1f530bba6abdbf159a183d11c3d
Marianna Ferreira <117746493+httpmarin2s@users.noreply.github.com>
2024-11-19 21:45:27 -0300

    Create code.py

---
+++ Introduction to Python/Section 001/Código 3/code.py
@@ -0,0 +1,6 @@
+#3. Escreva um programa que verifique se um número fornecido pelo usuário é par ou ímpar.
+numero = int(input("Digite um número: "))
+if numero % 2 == 0: 
+    print (f" O número {numero} é par")
+else: 
+    print(f" O número {numero} é e impar")
