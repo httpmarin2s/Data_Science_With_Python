67529aefde30423fa878e88e215f3a3e11ccb5be
Marianna Ferreira <117746493+httpmarin2s@users.noreply.github.com>
2024-11-19 22:46:37 -0300

    Create code.py

---
+++ Introduction to Python/Section 002 /Código 4 /code.py
@@ -0,0 +1,4 @@
+# 9. Crie uma lista contendo as palavras ["maçã", "banana", "laranja"]. Substitua a palavra "banana" por "uva".
+frutas = ["maça", "banana", "laranja", "uva"]
+frutas[1] = "uva"
+print(frutas)
