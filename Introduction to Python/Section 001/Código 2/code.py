768a80bfb52d8162049d16ad4aa0ad74b6d8c7f4
Marianna Ferreira <117746493+httpmarin2s@users.noreply.github.com>
2024-11-19 21:39:19 -0300

    Create code.py

---
+++ Introduction to Python/Section 001/Código 2/code.py
@@ -0,0 +1,7 @@
+# 2.** Crie uma variável para armazenar seu nome e outra para sua idade. Imprima uma mensagem no formato:
+# Olá, meu nome é [nome] e eu tenho [idade] anos."`
+
+a = input("Digite seu nome: ")
+b = int(input("Digite sua idade: "))
+
+print(f"Olá, meu nome é {a} e minha idade é de {b} anos ")
