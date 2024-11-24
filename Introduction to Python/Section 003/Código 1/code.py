0009a34caccaa0850e096d3dd76ed2c4ec754c8a
Marianna Ferreira <117746493+httpmarin2s@users.noreply.github.com>
2024-11-24 10:56:04 -0300

    Create code.py

---
+++ Introduction to Python/Section 003/Código 1/code.py
@@ -0,0 +1,7 @@
+def calcular_area_retangulo(x,y): 
+  area = x * y 
+  return area 
+
+base = int(input("Digite a base do retângulo: "))
+altura = int(input("Digite a altura do retângulo: "))
+calcular_area_retangulo(base,altura)
