0b2c882f49eac9bfb7088d1ad6ee4a960e17d9c9
Marianna Ferreira <117746493+httpmarin2s@users.noreply.github.com>
2024-11-24 10:56:40 -0300

    Delete Introduction to Python/Section 003/Código 1 directory

--- Introduction to Python/Section 003/Código 1/code.py
+++
@@ -1,7 +0,0 @@
-def calcular_area_retangulo(x,y): 
-  area = x * y 
-  return area 
-
-base = int(input("Digite a base do retângulo: "))
-altura = int(input("Digite a altura do retângulo: "))
-calcular_area_retangulo(base,altura)
