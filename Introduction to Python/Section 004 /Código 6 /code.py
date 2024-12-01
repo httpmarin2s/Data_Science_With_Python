e3dca76033fc4201b6fdaf91464f56209640a0bc
Marianna Ferreira <117746493+httpmarin2s@users.noreply.github.com>
2024-12-01 08:45:38 -0300

    Create code.py

---
+++ Introduction to Python/Section 004 /Código 6 /code.py
@@ -0,0 +1,9 @@
+import numpy as np
+x = np.random.randint(1,100, size=(3, 3))
+y = np.random.randint(1,100, size=(3, 3))
+
+print(vetor1)
+print("______")
+print(vetor2)
+print("______")
+print(x + y)
