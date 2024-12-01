48238fe833e4b4d5739537534462e959b2e8e5f0
Marianna Ferreira <117746493+httpmarin2s@users.noreply.github.com>
2024-12-01 08:48:52 -0300

    Create README.mk

---
+++ Introduction to Python/Section 005 /README.mk
@@ -0,0 +1,7 @@
+# Section 001
+Pasta para os códigos da seção 001.
+- [x]  **Código 1.** Escreva um programa que receba dois números do usuário e imprima a soma, subtração, multiplicação e divisão deles.
+- [x]  **Código 2.** Crie uma variável para armazenar seu nome e outra para sua idade. Imprima uma mensagem no formato: Olá, meu nome é [nome] e eu tenho [idade] anos."`
+- [x]  **Código 3.** Escreva um programa que verifique se um número fornecido pelo usuário é par ou ímpar.
+- [x]  **Código 4.** Escreva um laço que imprime os números de 1 a 10.
+- [x]  **Código 5.** Crie um programa que receba uma palavra do usuário e imprima cada letra da palavra separadamente usando um laço `for`.
